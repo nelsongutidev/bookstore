104bbc6ccac6f6eda2aa783f3d9bbba2c854db12
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-03-25 22:31:41 -0600

    initial commit

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about.component.html',
+  styles: [
+  ]
+})
+export class AboutComponent {
+
+}
---
+++ src/app/pages/events/events.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './events.component.html',
+  styles: [
+  ]
+})
+export class EventsComponent {
+
+}
