0841586de5ed3e7e14646fa417578f2244ec8921
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-03-26 23:36:34 -0600

    update import

--- src/app/pages/blog-post/blog-post.component.ts
+++ src/app/pages/blog-post/blog-post.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScullyLibModule } from '@scullyio/ng-lib';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [CommonModule, ScullyLibModule, RouterLink],
+  imports: [CommonModule, ScullyLibModule, RouterModule],
   templateUrl: './blog-post.component.html',
   styles: [],
 })
