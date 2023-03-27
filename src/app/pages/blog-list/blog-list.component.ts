8befef25dbcd8be07746f2829d64b19d1d8bfbdd
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-03-26 23:38:59 -0600

    update import on blog list

--- src/app/pages/blog-list/blog-list.component.ts
+++ src/app/pages/blog-list/blog-list.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
 import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterModule],
   templateUrl: './blog-list.component.html',
   styles: [],
 })
